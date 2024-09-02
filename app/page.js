b193ff19f094d537cc23c3f36e5a1c28bf9b188e
Nathan Petain <nejc.petan14@gmail.com>
2024-09-02 21:08:07 +0200

    Added a body component for services

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Body from "./components/body";
 import NavHero from "./components/navhero";
 
 export default function Home() {
   return (
     <main>
       <NavHero />
+      <Body />
     </main>
   );
 }
